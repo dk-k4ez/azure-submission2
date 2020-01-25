c3124f14fd2316af146a2abb264a015844b4e013
Deddy Kurniawan <dkurniawan232@gmail.com>
2020-01-25 13:35:36 +0700

    "first commit"

---
+++ computervision.php
@@ -0,0 +1,113 @@
+<?php
+if (isset($_POST['submit'])) {
+    if (isset($_POST['url'])) {
+        $url = $_POST['url'];
+    } else {
+        header("Location: index.php");
+    }
+} else {
+    header("Location: index.php");
+}
+?>
+
+<!DOCTYPE html>
+    <html>
+    <head>
+        <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+
+            <title>Analisis Gambar</title>
+
+            <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/starter-template/">
+
+            <!-- CSS -->
+            <link href="https://getbootstrap.com/docs/4.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+            <!-- Custom styles -->
+            <link href="starter-template.css" rel="stylesheet">
+
+            <script src="http://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    </head>
+        <body>
+        
+		<main role="main" class="container">
+    		<div class="starter-template"> <br><br><br>
+        		
+			</div>
+ 
+<script type="text/javascript">
+    function processImage() {
+        
+        var subscriptionKey = "fea2ac01ec8b471b9dae6960cb0ae36b";
+ 
+        var uriBase =
+            "https://southeastasia.api.cognitive.microsoft.com/vision/v2.0/analyze";
+ 
+        var params = {
+            "visualFeatures": "Description",
+            "details": "",
+            "language": "en",
+        };
+ 
+        var sourceImageUrl = document.getElementById("inputImage").value;
+        document.querySelector("#sourceImage").src = sourceImageUrl;
+ 
+        // Make the REST API call.
+        $.ajax({
+            url: uriBase + "?" + $.param(params),
+ 
+            // Request headers.
+            beforeSend: function(xhrObj){
+                xhrObj.setRequestHeader("Content-Type","application/json");
+                xhrObj.setRequestHeader(
+                    "Ocp-Apim-Subscription-Key", subscriptionKey);
+            },
+ 
+            type: "POST",
+ 
+            // Request body.
+            data: '{"url": ' + '"' + sourceImageUrl + '"}',
+        })
+ 
+        .done(function(data) {
+            // Show formatted JSON on webpage.
+            $("#responseTextArea").val(JSON.stringify(data, null, 2));
+        })
+ 
+        .fail(function(jqXHR, textStatus, errorThrown) {
+            // Display error message.
+            var errorString = (errorThrown === "") ? "Error. " :
+                errorThrown + " (" + jqXHR.status + "): ";
+            errorString += (jqXHR.responseText === "") ? "" :
+                jQuery.parseJSON(jqXHR.responseText).message;
+            alert(errorString);
+        });
+    };
+</script>
+ 
+<h1>Analisa Gambar menggunakan Azure Computer Vision</h1>
+Tekan tombol <strong>Analisis Gambar</strong> untuk memulai proses analisis gambar.
+<br><br>
+URL gambar:
+<input type="text" name="inputImage" id="inputImage"
+    value="<?php echo $url ?>" readonly />
+<button id="analyze_btn" onclick="processImage()" style="btn btn-danger">Analisis Gambar</button>
+<br><br>
+<script language="javascript">
+document.getElementById('analyze_btn').click(); 
+</script>
+<div id="wrapper" style="width:1020px; display:table;">
+    <div id="jsonOutput" style="width:600px; display:table-cell;">
+        Response:
+        <br><br>
+        <textarea id="responseTextArea" class="UIInput"
+                  style="width:580px; height:400px;"></textarea>
+    </div>
+    <div id="imageDiv" style="width:420px; display:table-cell;">
+        Source image:
+        <br><br>
+        <img id="sourceImage" width="400" />
+    </div>
+</div>
+</body>
+</html>
